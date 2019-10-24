a74356911c8a3f038cd93abe5382e76e5c9f3dd2
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-10-24 20:36:26 +1030

    Add express and ts-node-dev for dev

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
 const message = "Hello, world!";
 
-console.log(message);
+
+app.get('/', (req, res) => res.send(message));
+
+app.listen(port, () => console.log(`MCV Companion listening on port ${port}.`));
