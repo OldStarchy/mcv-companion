57b20d21aa2c9418ba17aa5ae0a9d83e5554ed00
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-10-26 15:32:29 +1030

    Add login/logout/whoami endpoints

--- server/src/api1.ts
+++ server/src/api1.ts
@@ -1,27 +1,67 @@
 import { IMinecraftClient } from './MinecraftClient';
 import { fail } from 'assert';
+import { stringify } from 'querystring';
 
 type ApiOptions = {
 	base: string;
 	client: IMinecraftClient;
+	authenticator: IAuthenticator;
 };
+
+export interface IAuthenticator {
+	checkPassword(username: string, password: string): boolean;
+}
+
+export class Authenticator implements IAuthenticator {
+	constructor(private client: IMinecraftClient) {}
+
+	checkPassword(username: string, password: string) {
+		if (this.client.whitelist.contains(username)) {
+			if (username === password) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+export class UserSession {
+	constructor(private session: Express.Session, private authenticator: IAuthenticator) {}
+
+	login(username: string, password: string) {
+		if (this.authenticator.checkPassword(username, password)) {
+			this.session.loggedIn = username;
+			return true;
+		}
+
+		return false;
+	}
+
+	currentUser() {
+		return this.session!.loggedIn;
+	}
+
+	logout() {
+		return new Promise(s => {
+			this.session!.destroy(s);
+		});
+	}
+}
 
 export class Api1 {
 	constructor(private readonly options: ApiOptions) {}
 
 	apply(app: import('express').Express) {
-		const { base, client } = this.options;
+		const { base, authenticator } = this.options;
 
 		app.post(`${base}/login`, (req, res) => {
 			const username = req.body.username;
+			const password = req.body.password;
 
-			if (!username) {
-				return res.send({
-					message: 'nope',
-				});
-			}
+			const userSession = new UserSession(req.session!, authenticator);
 
-			if (client.whitelist.contains(username)) {
+			if (userSession.login(username, password)) {
 				return res.send({
 					message: 'OK',
 				});
@@ -29,6 +69,23 @@ export class Api1 {
 
 			return res.send({
 				message: 'nope',
+			});
+		});
+
+		app.get(`${base}/logout`, (req, res) => {
+			const userSession = new UserSession(req.session!, authenticator);
+
+			userSession.logout().then(() =>
+				res.send({
+					message: 'OK',
+				}),
+			);
+		});
+
+		app.get(`${base}/whoami`, (req, res) => {
+			const userSession = new UserSession(req.session!, authenticator);
+			return res.send({
+				message: userSession.currentUser() || 'nobody',
 			});
 		});
 	}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { Api1 } from './api1';
+import { Api1, UserSession, Authenticator } from './api1';
 import { IMinecraftClient, MinecraftClient1v14, MinecraftRconClient } from './MinecraftClient';
+import session from 'express-session';
 
-const app = express();
+// TODO: move this kind of thing to config file
 const port = 3000;
 const message = 'Hello, world!';
 const clientRoot = '../client/dist/';
+const useHttps = false;
+const sessionSecret = "i don't know what this should be";
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(
 	express.static(clientRoot, {
 		// index: true,
 		extensions: ['html'],
+	}),
+);
+app.use(
+	session({
+		secret: sessionSecret,
+		resave: true,
+		saveUninitialized: false,
 	}),
 );
 
@@ -22,6 +34,7 @@ const client = new MinecraftClient1v14(new MinecraftRconClient('localhost', 9000
 new Api1({
 	client,
 	base: '/api/v1',
+	authenticator: new Authenticator(client),
 }).apply(app);
 
 app.get('/api', (req, res) => {
